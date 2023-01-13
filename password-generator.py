c3ba8c7827b14d949c7dbc0e512d3d149101d97d
Yasin Mohammadzade <103330832+YasinMhmmdzd@users.noreply.github.com>
2023-01-13 18:30:57 +0330

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -3,3 +3,12 @@ import pyperclip
 from PyQt6.QtWidgets import QApplication , QWidget , QLabel , QPushButton , QLineEdit
 import sys
 from PyQt6.QtGui import QIcon , QFont , QPixmap
+app = QApplication(sys.argv)
+class MainPasswordGeneratorWindow(QWidget):
+    def __init__(self):
+        super(MainPasswordGeneratorWindow, self).__init__()
+
+
+window = MainPasswordGeneratorWindow()
+window.show()
+sys.exit(app.exec())
